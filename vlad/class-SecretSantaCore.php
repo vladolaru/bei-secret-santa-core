6f2c103e41afa6c078b3eba1042e4d01f78df5ac
vladalar <vladpopa63@gmail.com>
2018-07-27 12:12:14 +0300

    First modification of the class

--- vlad/class-SecretSantaCore.php
+++ vlad/class-SecretSantaCore.php
@@ -1,5 +1,44 @@
 <?php
+class SecretSantaCore
+{
+    protected $FromEmail = null;
+    protected $EmailTitle = null;
+    protected $recommendedExpenses = null;
+    protected $users = array();
+    protected $sentEmails = array ();
 
-class SecretSantaCore {
+    public function setFromEmail ($email)
+    {
+        if (filter_var($email,FILTER_VALIDATE_EMAIL))
+        {
+            $this->fromEmail = $email;
+        }
+        else {
+            echo "The e-mail $email address is invaild!";
+        }
+    }
+    public function setEmailTitle ($subject)
+    {
+
+    }
+    public function setRecommendedExpenses ($recommended)
+    {
+        if (is_numeric ($recommended))
+        {
+            $this->recommendedExpenses = $recommended;
+        }
+    }
+
 
 }
+
+$santa = new SecretSantaCore();
+$santa->setMailFrom('test@test.com');
+$santa->setMailTitle();
+$santa->setRecommendedExpenses(); //trebuie sa fie float!!
+$santa->setUsers();
+$santa->run();
+$santa->getSentEmailsAddresses();
+
+
+
