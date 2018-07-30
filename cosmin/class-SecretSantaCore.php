cb509d927f749265b7c2786db96b98792525e329
BurloiuCosmin <cosmin.burloiu@yahoo.com>
2018-07-30 16:51:03 +0300

    solved the userSantaCheck function, to see if every function worked properly

--- cosmin/class-SecretSantaCore.php
+++ cosmin/class-SecretSantaCore.php
@@ -6,26 +6,24 @@ class SecretSantaCoreCosmin
     protected $emailTitle = 'NoTitle';
     protected $recommendedExpenses = 0;
     protected $users = array();
-    protected $userEmail = '';
+    protected $sentEmailAddresses=array();
 
 
     public function setFromEmail($email)
     {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $this->fromEmail = $email;
-            return 1;
         } else
             //echo "The email from which the emails will be sent is not a valid one!";
-        return 0;
+        return false;
     }
 
     public function setRecommendedExpenses($sum)
     {
         if (is_numeric($sum) && $sum>0) {
             $this->recommendedExpenses = $sum;
-            return 1;
         } else
-            return 0;
+            $this->recommendedExpenses = 0; //return false;
     }
 
     public function setEmailTitle($title)
@@ -34,59 +32,52 @@ class SecretSantaCoreCosmin
         return 1;
     }
 
-    public function getSentEmailAddresses($users)
+    public function getSentEmailAddresses()
     {
-        foreach ($users as $key => $user) {
-            return $user['email'];
-        }
+         return $this->sentEmailAddresses;
     }
 
-    public function addUsers($users)
+    !!!! public function addUsers($users)
     {
-        $users = array ('users' => array ('name', 'email'));
+        array_push($this->users, array( 'name' => , 'email' => $email));
+
     }
 
     public function randomizeUsers($users)
     {
             $firstUser = 0;
-            $lastUser = count($users) - 1;
+            $lastUser = count($this->users) - 1;
             return rand($firstUser, $lastUser);
     }
 
     public function userSantaCheck()
     {
-
         if (count($this->users) <= 2 && count($this->users) >= 0) {
             //echo "Very few users added, result is obvious";
-            return 0;
+            return false;
         }
-        if()
-
-
-
-
-        $msg='Dear' . ' ' . $user1 . ', ' . ' the person you will have to give a gift to is' . ' ' . $user2 . ', ' .
-            'and the gift value is' . ' ' . $recommendedExpenses . ' USD';
-        echo $msg;
-
+        if($this->fromEmail=='' || $this->recommendedExpenses==0)
+        {
+            return false;
+        }
+        else
+            return true;
     }
 
     public function goRudolph()
     {
-        //sa nu uitam sa verificam ca avem toate valorile valide
-        if($this->userSantaCheck()==0)
+        if($this->userSantaCheck()===true)
         {
-            echo "You missed something!";
+            array_push($this->getSentEmailAddresses(), $this->users['email']);
         }
+        else
+            echo "Not all the information written is correct, please retry!";
     }
 
 
 
-    }
 
 
 }
 
-//de gandit algoritmul in care punem in array-ul nostru
-//cum luam datele specifice, cum ar fi adresele la care s-au trimis emailurile date de noi
-//algoritmul care verifica validitatea userilor in momentul imperecherii si mesajul necesar
+
