2979c96f0e81c3fdb75a35824d6ff1cef6d83f6c
BurloiuCosmin <cosmin.burloiu@yahoo.com>
2018-07-30 17:53:47 +0300

    fixed some obvious syntax errors

--- cosmin/class-SecretSantaCore.php
+++ cosmin/class-SecretSantaCore.php
@@ -6,7 +6,8 @@ class SecretSantaCoreCosmin
     protected $emailTitle = 'NoTitle';
     protected $recommendedExpenses = 0;
     protected $users = array();
-    protected $sentEmailAddresses=array();
+    protected $sentEmailAddresses = array();
+    protected $pairing = array();
 
 
     public function setFromEmail($email)
@@ -15,12 +16,12 @@ class SecretSantaCoreCosmin
             $this->fromEmail = $email;
         } else
             //echo "The email from which the emails will be sent is not a valid one!";
-        return false;
+            return false;
     }
 
     public function setRecommendedExpenses($sum)
     {
-        if (is_numeric($sum) && $sum>0) {
+        if (is_numeric($sum) && $sum > 0) {
             $this->recommendedExpenses = $sum;
         } else
             $this->recommendedExpenses = 0; //return false;
@@ -29,25 +30,24 @@ class SecretSantaCoreCosmin
     public function setEmailTitle($title)
     {
         $this->emailTitle = $title;
-        return 1;
     }
 
     public function getSentEmailAddresses()
     {
-         return $this->sentEmailAddresses;
+        return $this->sentEmailAddresses;
     }
 
-    !!!! public function addUsers($users)
+    public function addUsers($users)
     {
-        array_push($this->users, array( 'name' => , 'email' => $email));
+        array_push($this->users, array('name' => $user[0], 'email' => $user[1]));
 
     }
 
     public function randomizeUsers($users)
     {
-            $firstUser = 0;
-            $lastUser = count($this->users) - 1;
-            return rand($firstUser, $lastUser);
+        $firstUser = 0;
+        $lastUser = count($this->users) - 1;
+        return rand($firstUser, $lastUser);
     }
 
     public function userSantaCheck()
@@ -56,26 +56,36 @@ class SecretSantaCoreCosmin
             //echo "Very few users added, result is obvious";
             return false;
         }
-        if($this->fromEmail=='' || $this->recommendedExpenses==0)
-        {
+        if ($this->fromEmail == '' || $this->recommendedExpenses == 0) {
             return false;
-        }
-        else
+        } else
             return true;
     }
 
+    //folosind functia random, punem pe toti cei care au fost deja selectati sa primeasca un cadou,
+    //sa nu fie realesi
+!!!!protected function doPairing()
+{
+
+
+}
+
+//verificam ca 2 useri sa nu aiba acelasi e-mail
+!!!!protected function doubleCheckUsersEmail()
+{
+
+}
+
     public function goRudolph()
     {
-        if($this->userSantaCheck()===true)
-        {
-            array_push($this->getSentEmailAddresses(), $this->users['email']);
-        }
-        else
+        if ($this->userSantaCheck() == true) {
+            foreach ($this->users as $user)
+                array_push($this->getSentEmailAddresses(), $this->users['email']);
+            ////
+            pairing($this->users);
+        } else
             echo "Not all the information written is correct, please retry!";
     }
-
-
-
 
 
 }
